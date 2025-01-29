dc58abcf37b00bd833f25ed8796a6bccf18df410
Wannes Michielsen-Meulemans <wannesmm@gmail.com>
2025-01-29 14:52:55 +0100

    Safety commit
    Ik ga de helft van de code in cardLayout verplaatsen naar battlefield omdat het heel onlogisch is dat deze in cardLayout staat.

--- Scenes/battlefield.gd
+++ Scenes/battlefield.gd
@@ -1,17 +1,9 @@
-extends Node2D
+extends Control
 
-# Constants for card size in pixels (A4 vertical size in your game) 210
-var ARRAY_WIDTH = 700
-var CENTER_Y = 150
-var CENTER_X = 0
-var CARD_SCALE = 1
-var ADDEDCARDS = []
-var CARD_LAYOUT_TYPE = "AllyLayout"
 const ANIMATIONSCALE = Vector2(1.19,1.19)
 const ANIMATIONDURATION: float = 0.19
 const SELECTMOVEMENT = Vector2(0,-25)
 const HOLDTRESHHOLD = 0.15
-var anchorPosition: Vector2 = Vector2(0,0)
 
 var cardBeingDragged
 var selectedCard
@@ -19,31 +11,19 @@ var screenSize
 var isPressed = false
 var holdTimer = 0.0
 
-signal signalAddExistingCard
-
 func _ready():
 	screenSize = get_viewport_rect().size
-	assignConstants()
-	addInitialCards()
-	arrange_cards()
 	
 func _process(delta: float) -> void:
 	cardFollowMouse()
 	clickAndHoldLogic(delta)
-	
-func assignConstants():
-	ARRAY_WIDTH = ARRAY_WIDTH
-	CENTER_Y = CENTER_Y
-	CENTER_X = CENTER_X
-	CARD_SCALE = CARD_SCALE
-	CARD_LAYOUT_TYPE = CARD_LAYOUT_TYPE
 	
 func connectSignal(card):
 	if card.has_signal("cardMouseEntered"):
 		card.connect("cardMouseEntered", Callable(self, "cardMouseEntered"))
 	if card.has_signal("cardMouseExited"):
 		card.connect("cardMouseExited", Callable(self, "cardMouseExited"))
-	
+
 func cardMouseEntered(card):
 	if !cardBeingDragged:
 		card.highlightCard()
@@ -54,68 +34,6 @@ func cardMouseExited(card):
 	card.undoHighlightCard()
 	card.scaleRelative(Vector2(1,1), ANIMATIONDURATION)
 	#card.moveCardDownSelect()
-	
-# Arrange Node2D cards dynamically
-func arrange_cards():
-	var num_cards = get_child_count()
-	if num_cards == 0:
-		return
-	
-	var arrayStart = 0 - (ARRAY_WIDTH / 2)
-	var cardOffset = ARRAY_WIDTH / (num_cards + 1)
-	# Position each card
-	for i in range(num_cards):
-		var card = get_child(i)
-		if card is Node2D:
-			card.position = Vector2(arrayStart + ((i + 1) * cardOffset) + CENTER_X , CENTER_Y)
-			card.setBasePosition(card.position)
-			#print("my position is " + str(card.position))
-
-func returnToBasePosition(card):
-	card.position = card.getBasePosition()
-
-func addCard(cardLogic):
-	var cardScene = load("res://Scenes/card.tscn")
-	
-	if cardScene is PackedScene:  # Ensure it is a PackedScene
-		var newCard = cardScene.instantiate()  # Create an instance of the card scene
-		
-		newCard.setCard(cardLogic)
-		
-		newCard.assignDefaultScale(Vector2(CARD_SCALE,CARD_SCALE))
-		connectSignal(newCard)
-		
-		add_child(newCard)  # Add the new card to the parent node
-		ADDEDCARDS.append(newCard)
-		
-		arrange_cards()  # Optionally call your arrange function to reposition cards
-	else:
-		print("Failed to load the card scene!")
-	
-func addExistingCard(card):
-	add_child(card)  # Add the new card to the parent node
-	ADDEDCARDS.append(card)
-		
-	arrange_cards()
-	
-func addCardToLayout(card, cardSlot):
-	var layout = cardSlot.getRespectiveCardLayout()
-	remove_child(card)
-	ADDEDCARDS.erase(card)
-	layout.addExistingCard(card)
-	arrange_cards()
-	
-func moveCardBasePosition(card):
-	card.position = card.getBasePosition()
-	
-func addInitialCards():
-	var greon = NomaGreon.new()
-	var bartholomew = BartholomewBalderstone.new()
-	var torinn = TorinnInn.new()
-	
-	addCard(greon)
-	addCard(bartholomew)
-	addCard(torinn)
 	
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
