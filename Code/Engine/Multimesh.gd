c0283575e1dfa272a2d832b07f6275bfe317a649
Wannes Michielsen-Meulemans <wannesmm@gmail.com>
2025-05-03 01:02:34 +0200

    Performance improvements at the cost of lighting changes

--- Code/Engine/Multimesh.gd
+++ Code/Engine/Multimesh.gd
@@ -1,10 +1,15 @@
 extends MultiMeshInstance3D
 
+@export var area_size: Vector3 = Vector3(50.0, 0.0, 50.0)
+
 func _ready():
 	if multimesh == null:
 		return
-	
 	for i in range(multimesh.instance_count):
 		var transform = Transform3D.IDENTITY
-		transform.origin = Vector3(randf() * 50.0, 0, randf() * 50.0)  # Random position
+		transform.origin = Vector3(
+			randf() * area_size.x - area_size.x / 2.0,
+			0, # Assuming you want trees at Y=0
+			randf() * area_size.z - area_size.z / 2.0
+		)
 		multimesh.set_instance_transform(i, transform)
